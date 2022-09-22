e552f3d30672ba9efdcb7f341fda83bb7ac2f40f
unknown <wisejohn950330@gmail.com>
2022-09-22 18:25:27 +0900

    상품 Entity 설계

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController				// Restful Web API를 좀 더 쉽게 만들기위한 어노테이션
 @SpringBootApplication
-public class DemoApplication {
+public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ShopApplication.class, args);
 	}
 
 	@GetMapping(value = "/")
--- src/main/java/com/shop/UserDto.java
+++ src/main/java/com/shop/UserDto.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.shop;
 
 import lombok.Getter;
 import lombok.Setter;
