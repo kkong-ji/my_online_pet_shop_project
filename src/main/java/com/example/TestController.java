e2d5f3c2729670e8b7b9fce54f08de1bbe4d1400
unknown <wisejohn950330@gmail.com>
2022-09-22 16:48:46 +0900

    프로젝트 개발 환경 구축

---
+++ src/main/java/com/example/TestController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public UserDto test() {
+
+        UserDto userDto = new UserDto();
+        userDto.setAge(20);
+        userDto.setName("han");
+
+        return userDto;
+    }
+}
---
+++ src/main/java/com/example/UserDto.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private Integer age;
+}
