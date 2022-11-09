aa88222d5bbc901c48f9cb586b867de4817f025b
unknown <wisejohn950330@gmail.com>
2022-11-09 10:29:35 +0900

    페이지 권한 설정하기

--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -42,7 +42,7 @@ public class Member {
         member.setAddress(memberFormDto.getAddress());
         String password = passwordEncoder.encode(memberFormDto.getPassword());
         member.setPassword(password);
-        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN);
         return member;
     }
 }
