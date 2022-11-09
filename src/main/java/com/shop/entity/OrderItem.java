bd90714ee73f7dbb3baee115b6d6e58c0b069844
unknown <wisejohn950330@gmail.com>
2022-11-09 14:53:33 +0900

    장바구니 엔티티 설계 & 연관관계 매핑

---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,35 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
---
+++ src/main/java/com/shop/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.shop.repository;
+
+import com.shop.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
