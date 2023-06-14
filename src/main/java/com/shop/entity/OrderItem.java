13dd77e2c2b29ad5c17ceb1886febfdf39cfff34
unknown <wisejohn950330@gmail.com>
2023-06-14 14:33:15 +0900

    Navbar와 Item 상세정보 수정

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -1,11 +1,9 @@
 package com.shop.entity;
 
-import com.shop.constant.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
-import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
